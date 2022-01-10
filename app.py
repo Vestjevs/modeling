a526ed4c490dba0157299b4b3f23254e14b03e4f
Jevs <38396669+Vestjevs@users.noreply.github.com>
2022-01-10 23:15:39 +0300

    update main page with test graphic

--- app.py
+++ app.py
@@ -5,14 +5,31 @@ import numpy as np
 
 import modeling as mod
 
-t = np.arange(1, 2, .01)
-p_r_t = np.array([mod.p(x) for x in t])
+elements = [mod.Element(1.5, 1.7, 1.2, 0.5),
+            mod.Element(1.1, 1.5, 0.7, 0.4),
+            mod.Element(1.1, 1.2, 0.6, 0.9),
+            mod.Element(2.1, 2.3, 0.2, 1.1)]
+
+r_i = [i for i in range(3, 23)]
+t_i = [i for i in range(2, 22)]
+
+p_0 = 1.2
+
+
+def p():
+    p = []
+    for r, t in zip(r_i, t_i):
+        for elem in elements:
+            elem.generate_q()
+        p.append(mod.P(r, t, p_0, elements, [i for i in range(1, t)]))
+    return p
+
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div(
     children=[
-        html.H1(children="Моделирование",),
+        html.H1(children="Моделирование", ),
         html.P(
             children="Моделирование задачи",
         ),
@@ -20,8 +37,8 @@ app.layout = html.Div(
             figure={
                 "data": [
                     {
-                        "x": t,
-                        "y": p_r_t,
+                        "x": t_i,
+                        "y": p(),
                         "type": "lines",
                     },
                 ],
@@ -33,4 +50,4 @@ app.layout = html.Div(
 
 if __name__ == "__main__":
     app.run_server(debug=True,
-                   host = '127.0.0.1')
+                   host='127.0.0.1')
